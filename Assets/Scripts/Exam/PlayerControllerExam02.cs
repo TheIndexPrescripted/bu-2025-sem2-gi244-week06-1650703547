2a39f8e58547bff19a3131b1f34936915cde57f3
TheIndexPrescripted <akkanat.wong@bumail.net>
2026-02-20 16:30:23 +0700

    1,2,3ExamDone

--- Assets/Scripts/Exam/PlayerControllerExam02.cs
+++ Assets/Scripts/Exam/PlayerControllerExam02.cs
@@ -21,5 +21,21 @@ public class PlayerControllerExam02 : MonoBehaviour
     void Update()
     {
         verticalInput = moveAction.ReadValue<Vector2>().y;
+
+        transform.Translate(-verticalInput * speed * Time.deltaTime * Vector3.right);
+
+        if (transform.position.z < -zRange)
+        {
+            transform.position = new Vector3(-zRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(zRange, transform.position.y, transform.position.z);
+        }
+        if (shootAction.triggered)
+        {
+            // [13] spawn a projectile
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
--- Assets/Scripts/Exam/PlayerControllerExam03.cs
+++ Assets/Scripts/Exam/PlayerControllerExam03.cs
@@ -14,18 +14,20 @@ public class PlayerControllerExam03 : MonoBehaviour
     private InputAction moveAction;
     private InputAction shootAction;
 
+    private float autoFireTimer;   // ? ตัวจับเวลา
+
     private void Awake()
     {
         moveAction = InputSystem.actions.FindAction("Move");
         shootAction = InputSystem.actions.FindAction("Shoot");
     }
 
-    // Update is called once per frame
     void Update()
     {
         horizontalInput = moveAction.ReadValue<Vector2>().x;
         transform.Translate(horizontalInput * speed * Time.deltaTime * Vector3.right);
 
+        // จำกัดขอบเขตการเคลื่อนที่
         if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
@@ -35,9 +37,25 @@ public class PlayerControllerExam03 : MonoBehaviour
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
 
-        if (shootAction.triggered)
+        // ? ระบบ Auto Fire
+        if (enableAutoFireMode)
         {
-            Instantiate(projectilePrefab, transform.position, transform.rotation);
+            autoFireTimer += Time.deltaTime;
+
+            if (autoFireTimer >= autoFireInterval)
+            {
+                Instantiate(projectilePrefab, transform.position, transform.rotation);
+                autoFireTimer = 0f;
+            }
+        }
+        else
+        {
+            // ยิงปกติเมื่อกดปุ่ม
+            if (shootAction.triggered)
+            {
+                Instantiate(projectilePrefab, transform.position, transform.rotation);
+            }
         }
     }
 }
+
